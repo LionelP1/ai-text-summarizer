c5d46440b404cc657b145ecf7a9f35242719dbcf
Lionel <lpei1900@gmail.com>
2025-01-02 22:33:52 -0500

    Updated the generate content module

--- services/generateContent.js
+++ services/generateContent.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY; 
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: " gemini-1.5-flash"});
 
-const prompt = "How does ai work?";
-const result = await model.generateContent(prompt);
-console.log(result.response.text);
+async function generateSummary(text) {
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const result = await model.generateContent({ prompt: text });
+    return result.response.text;
+}
+
+module.exports = { generateSummary };
